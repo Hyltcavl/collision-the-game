9261680c3d98aa8369a20d94bf571c3211635774
Teodor Englund <teodor.englund@hm.com>
2022-05-20 19:58:49 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css'
+import ''
+
+const [gameGrid, setGameGrid] = useState([])
+
+const grid = [
+  [0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,1,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0,0],
+]
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button>New Game</button>
+      <div className="game-grid">
+      {gameGrid.map(card => )}
+      </div>
+    </div>
+  );
+}
+
+export default App
